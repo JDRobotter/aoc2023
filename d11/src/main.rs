626eaaca1b3ef4bab16267f114254df17e7a6fca
JD <jeandamien.brossillon@gmail.com>
2023-12-23 22:45:33 +0100

    d13 done

--- d11/src/main.rs
+++ d11/src/main.rs
@@ -6,24 +6,25 @@ struct CombinePairs {
 
 impl CombinePairs {
     pub fn new(n: usize) -> Self {
-        Self { i: 0, j: 0, n }
+        Self { i: 0, j: 1, n }
     }
 }
 
 impl Iterator for CombinePairs {
     type Item = (usize, usize);
     fn next(&mut self) -> Option<Self::Item> {
+        if self.j == self.n {
+            return None;
+        }
+        let (i, j) = (self.i, self.j);
         // do a simple for i { for j {} } combination
         self.i += 1;
         // only count numbers below diagonal
-        if self.i == (self.j + 1) {
+        if self.i == (self.j) {
             self.i = 0;
             self.j += 1;
-            if self.j == self.n {
-                return None;
-            }
         }
-        Some((self.i, self.j))
+        Some((i, j))
     }
 }
 
@@ -94,6 +95,9 @@ struct Universe {
     height: usize,
     tiles: Vec<char>,
     galaxies: Vec<XY>,
+    expansion: usize,
+    empty_x: Vec<usize>,
+    empty_y: Vec<usize>,
 }
 
 impl Universe {
@@ -103,6 +107,9 @@ impl Universe {
             height: 0,
             tiles: Vec::new(),
             galaxies: Vec::new(),
+            expansion: 0,
+            empty_x: Vec::new(),
+            empty_y: Vec::new(),
         }
     }
 
@@ -151,7 +158,9 @@ impl Universe {
         self.tiles[k]
     }
 
-    pub fn expand(&mut self) {
+    pub fn expand(&mut self, expansion: usize) {
+        self.expansion = expansion;
+
         let mut empty_y = vec![];
         // iterate through lines to find empty ones
         for y in 0..self.height {
@@ -159,6 +168,7 @@ impl Universe {
                 empty_y.push(y);
             }
         }
+        self.empty_y = empty_y;
         // iterate through columns to find empty ones
         let mut empty_x = vec![];
         for x in 0..self.width {
@@ -166,33 +176,7 @@ impl Universe {
                 empty_x.push(x);
             }
         }
-
-        let nheight = self.height + empty_y.len();
-        let nwidth = self.width + empty_x.len();
-        let mut ntiles: Vec<char> = vec![];
-        for y in 0..self.height {
-            for x in 0..self.width {
-                let here = XY::new(x as isize, y as isize);
-                // this column is an empty one, push one more space
-                if empty_x.contains(&x) {
-                    ntiles.push('.');
-                    ntiles.push('.');
-                } else {
-                    ntiles.push(self.get_tile(here));
-                }
-            }
-
-            // this line is an empty one push one more line
-            if empty_y.contains(&y) {
-                for _ in 0..nwidth {
-                    ntiles.push('.');
-                }
-            }
-        }
-
-        self.height = nheight;
-        self.width = nwidth;
-        self.tiles = ntiles;
+        self.empty_x = empty_x;
     }
 
     pub fn count_galaxies(&mut self) {
@@ -201,6 +185,8 @@ impl Universe {
                 let here = XY::new(x as isize, y as isize);
 
                 if self.get_tile(here) == '#' {
+                    let i = self.galaxies.len();
+                    self.set_tile(here, char::from_digit(i as u32, 10).unwrap());
                     self.galaxies.push(here);
                 }
             }
@@ -208,19 +194,59 @@ impl Universe {
         println!("{:?}", self.galaxies);
     }
 
-    pub fn distance_between(&self, a: usize, b: usize) -> isize {
-        grid_distance(self.galaxies[a], self.galaxies[b])
+    pub fn distance_between(&self, a: usize, b: usize) -> usize {
+        //   EE    - X ->
+        //   123
+        //  #||
+        // E-||---
+        //   ||#
+        //
+
+        if a == b {
+            return 0;
+        }
+
+        let ga = self.galaxies[a];
+        let gb = self.galaxies[b];
+
+        let mut empties = 0;
+        // find empty spaces inside ga and gb along x
+        empties += self
+            .empty_x
+            .iter()
+            .filter(|&&x| {
+                let x = x as isize;
+                ga.x < x && x < gb.x
+            })
+            .count();
+        // find empty spaces inside ga and gb along y
+        empties += self
+            .empty_y
+            .iter()
+            .filter(|&&y| {
+                let y = y as isize;
+                ga.y < y && y < gb.y
+            })
+            .count();
+
+        let delta = gb - ga;
+
+        let distance = delta.x.abs() + delta.y.abs();
+        distance as usize + (empties * (self.expansion - 1))
     }
 
-    pub fn answer1(&self) -> isize {
+    pub fn answer1(&self) -> usize {
         let mut sum = 0;
         let n = self.galaxies.len();
         // for each pair of galaxies
+        let mut i = 0;
         for (a, b) in CombinePairs::new(n) {
             let distance = self.distance_between(a, b);
-            sum += distance;
             println!("{a} {b} {distance}");
+            sum += distance;
+            i += 1;
         }
+        println!("i={i}");
         sum
     }
 }
@@ -231,23 +257,38 @@ fn main() {
     assert_eq!(grid_distance(XY::new(0, 0), XY::new(0, 10)), 10);
     assert_eq!(grid_distance(XY::new(10, 0), XY::new(0, 0)), 10);
 
-    /*
     let data = "...#......
-    .......#..
-    #.........
-    ..........
-    ......#...
-    .#........
-    .........#
-    ..........
-    .......#..
-    #...#.....";
-    */
-    let mut data = String::new();
-    let mut file = std::fs::File::open("input").unwrap();
-    use std::io::Read;
-    file.read_to_string(&mut data).unwrap();
+                .......#..
+                #.........
+                ..........
+                ......#...
+                .#........
+                .........#
+                ..........
+                .......#..
+                #...#.....";
 
+    /*
+    11 + 3
+    ....1........
+    .........2...
+    3............
+    .............
+    .............
+    ........4....
+    .5...........
+    .##.........6
+    ..##.........
+    ...##........
+    ....##...7...
+    8....9.......
+    */
+    /*
+        let mut data = String::new();
+        let mut file = std::fs::File::open("input").unwrap();
+        use std::io::Read;
+        file.read_to_string(&mut data).unwrap();
+    */
     let mut f = Universe::new();
     for line in data.split('\n') {
         let line = line.trim();
@@ -258,9 +299,14 @@ fn main() {
     }
 
     f.print();
-    f.expand();
-    f.print();
+    f.expand(2);
     f.count_galaxies();
+    f.print();
+
+    assert_eq!(f.distance_between(0, 1), 6);
+    assert_eq!(f.distance_between(0, 6), 15);
+    assert_eq!(f.distance_between(2, 5), 17);
+    assert_eq!(f.distance_between(7, 8), 5);
 
     let ans1 = f.answer1();
     println!("ans1 = {ans1}");
